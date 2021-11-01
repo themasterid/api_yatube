dc314719a001284416b406aa2393224999a9e568
Klepikov Dmitriy <thebrootos@gmail.com>
2021-11-01 18:09:11 +0500

    all kettik bastayik 01.11.2021

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,10 +1,8 @@
 import os
 
-from .secrets import SECRET
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = SECRET
+SECRET_KEY = os.getenv("SECRET_KEY", default="SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT")
 
 DEBUG = True
 
