4c8689e1b2a060a7be17cd6a3f7de46b2f75a6fc
Klepikov Dmitriy <thebrootos@gmail.com>
2021-11-01 17:26:46 +0500

    all kettik bastayik 01.11.2021

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(f'{BASE_DIR}/secret_settings.txt') as f:
+with open(f'{BASE_DIR}\\secret_settings.txt') as f:
     SECRET_KEY = f.read().strip()
 
 DEBUG = True
