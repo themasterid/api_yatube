ce9a4710112f59c443e17cda0e60fa57f965b2ec
themasterid <thebrootos@gmail.com>
2022-07-23 03:24:16 +0500

    add func 23.07.2022

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -11,6 +11,10 @@ class Group(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
+    class Meta:
+        verbose_name_plural = 'Группы'
+        verbose_name = 'Группа'
+
     def __str__(self):
         return self.title
 
@@ -20,18 +24,29 @@ class Post(models.Model):
 
     text = models.TextField()
     pub_date = models.DateTimeField(
-        'Дата публикации', auto_now_add=True
+        'Дата публикации',
+        auto_now_add=True
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts'
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
     )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True
+        upload_to='posts/',
+        null=True,
+        blank=True
     )
     group = models.ForeignKey(
-        Group, on_delete=models.CASCADE,
-        related_name="posts", blank=True, null=True
+        Group,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank=True, null=True
     )
+
+    class Meta:
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
 
     def __str__(self):
         return self.text
@@ -52,3 +67,11 @@ class Comment(models.Model):
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
+
+    class Meta:
+        verbose_name_plural = 'Комментарии'
+        verbose_name = 'Комментарий'
+
+
+    def __str__(self):
+        return self.text
