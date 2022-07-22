512afef08b1defdfb735bcadc31e276c8eb11309
themasterid <thebrootos@gmail.com>
2022-07-23 02:42:01 +0500

    edit readme

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -15,5 +15,7 @@ router.register(
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('v1/', include(router.urls)),
 ]
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -2,7 +2,9 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
 
 DEBUG = True
 
